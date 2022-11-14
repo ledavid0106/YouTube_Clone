0a3289b531477397106f41c96a6ee33305d3f766
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-11-14 09:19:10 -0800

    Initiated views for comment

--- backend/Comment/views.py
+++ backend/Comment/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Comment
+from rest_framework import status
+from .serializers import CommentSerializer
 
-# Create your views here.
-
+@api_view(['GET'])
+def comment_list(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    if request.method == 'GET':
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
