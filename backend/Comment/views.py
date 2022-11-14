d323fd17098bf7d5f34cd382a42bf34b1c0de1d1
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-11-14 09:41:23 -0800

    Authentication equipment

--- backend/Comment/views.py
+++ backend/Comment/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .models import Comment
 from rest_framework import status
 from .serializers import CommentSerializer
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def comment_list(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     if request.method == 'GET':
