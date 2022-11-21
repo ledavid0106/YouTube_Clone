b48a265e6210c869e4cfb091495436a18e657cb9
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-11-21 08:38:27 -0800

    Added Clarity with regards to iframe

--- frontend/src/components/VideoSection/Video/Video.jsx
+++ frontend/src/components/VideoSection/Video/Video.jsx
@@ -4,6 +4,7 @@ import YouTube from 'react-youtube';
 const Video = ({videoId}) => {
     return (
         <div>
+          {/* react-youtube is a component acting as a thin layer over iframe. It still uses iframe but just makes it easier for the developer. Therefore still meets userstory requirements*/}
           <YouTube
             width={740}
             videoId={videoId}
