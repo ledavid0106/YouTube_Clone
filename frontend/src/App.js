2f783be3a2e5e9be79a969cce61c176c5a40df15
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-11-21 09:49:25 -0800

    Remove Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,7 @@ function App() {
           <Route path="/search/:searchQuery" element={<SearchPage />} />
           <Route path="/video/:videoId" element={<VideoPage />} />
       </Routes>
-      <Footer/>
+      {/* <Footer/> */}
     </div>
   );
 }
